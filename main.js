1cee9b5a78e1d2b97fd8a924153d2d3327cd934d
talaalosili <talaalosili@gmail.com>
2024-06-27 07:50:42 +0300

    tala

---
+++ main.js
@@ -0,0 +1,87 @@
+// //1
+// localStorage.setItem("hel", "Hello, World!");
+// console.log(localStorage.getItem("hel"));
+// //2
+// localStorage.setItem("num", 123)
+// console.log(localStorage.getItem("num"));
+
+// //3
+// localStorage.setItem("bool", "true")
+// console.log(localStorage.getItem("bool"));
+// //4
+// localStorage.setItem("item", "This is a test item")
+// console.log(localStorage.getItem("item"));
+// localStorage.removeItem("item")
+
+// //5
+// localStorage.setItem("item1", "This is the first item")
+// localStorage.setItem("item2", "This is the second item")
+// localStorage.setItem("item3", "This is the third item")
+// console.log(localStorage.getItem("item1"));
+// console.log(localStorage.getItem("item2"));
+// console.log(localStorage.getItem("item3"));
+// localStorage.clear()
+
+// //6
+// localStorage.setItem("name", "tala")
+// if (localStorage.getItem("name")) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+// console.log(localStorage.getItem("name"))
+// //7
+// localStorage.setItem("arr", JSON.stringify([1, 2, 3]));
+
+// console.log((localStorage.getItem("arr")))
+
+// //8
+// localStorage.user = JSON.stringify({
+//     name: "John", age: 30,
+// })
+// //9
+// localStorage.setItem("1", "items1")
+// localStorage.setItem("1", "ite")
+// localStorage.setItem("2", "items2")
+// localStorage.setItem("3", "items3")
+// console.log(localStorage.getItem("1"));
+// console.log(localStorage.getItem("2"));
+// console.log(localStorage.getItem("3"));
+
+//10
+// sessionStorage.setItem("myItem", "Updated Value");
+// const updatedValue = sessionStorage.getItem("myItem");
+// console.log(updatedValue);
+
+
+// sessionStorage.setItem("hel", "Hello, World!");
+// console.log(sessionStorage.getItem("hel"));
+
+// sessionStorage.setItem("num", 123)
+// console.log(sessionStorage.getItem("num"));
+
+
+// sessionStorage.setItem("bool", "true")
+// console.log(sessionStorage.getItem("bool"));
+
+// sessionStorage.setItem("item", "This is a test item")
+// console.log(sessionStorage.getItem("item"));
+// sessionStorage.removeItem("item")
+
+
+// sessionStorage.setItem("item1", "This is the first item")
+// sessionStorage.setItem("item2", "This is the second item")
+// sessionStorage.setItem("item3", "This is the third item")
+// console.log(sessionStorage.getItem("item1"));
+// console.log(sessionStorage.getItem("item2"));
+// console.log(sessionStorage.getItem("item3"));
+// sessionStorage.clear()
+
+
+// sessionStorage.setItem("1", "items1")
+// sessionStorage.setItem("2", "items2")
+// sessionStorage.setItem("3", "items3")
+// console.log(sessionStorage.getItem("1"));
+// console.log(sessionStorage.getItem("2"));
+// console.log(sessionStorage.getItem("3"));
+
